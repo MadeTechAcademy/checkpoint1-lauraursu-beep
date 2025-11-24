2083b29f41fc33ca966864f7cec6248d5124eeb6
laura ursu <laura.ursu@madetech.com>
2025-11-24 13:42:20 +0000

    Test if there are any Dublicates

--- test_themes.py
+++ test_themes.py
@@ -2,15 +2,16 @@ import unittest
 from themes import List_of_Duty
 
 class TestThemes(unittest.TestCase):
-
     
+    def test_duty_has_no_dublicates(self):
+        self.assertEqual(len(List_of_Duty), len(set(List_of_Duty)))
+
     def test_duties_count(self):
         self.assertEqual(len(List_of_Duty), 13)
 
     def test_duty_contents(self):
         for i, duty in enumerate(List_of_Duty, start=1):
             self.assertTrue(duty.startswith(f"Duty {i}"))
-    
 
 if __name__ == "__main__":
     unittest.main()
