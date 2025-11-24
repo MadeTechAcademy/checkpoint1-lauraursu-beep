544364725027b516bb0b410b96fe41e7dd9662f3
laura ursu <laura.ursu@madetech.com>
2025-11-24 12:59:59 +0000

    Check Duty 2 is there

--- test_themes.py
+++ test_themes.py
@@ -9,6 +9,12 @@ class TestThemes(unittest.TestCase):
         Duty_1 = "Duty 1 Script and code in at least one general purpose language and at least one domain-specific language to orchestrate infrastructure, follow test driven development and ensure appropriate test coverage."
         self.assertEqual(List_of_Duty[0], Duty_1)
     
+    def test_duty_2(self):
+        Duty_2 = "Duty 2 Initiate and facilitate knowledge sharing and technical collaboration with teams and individuals, with a focus on supporting development of team members."
+        self.assertEqual(List_of_Duty[1], Duty_2)
+    
+    
+    
 
 
 
