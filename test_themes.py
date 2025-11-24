ee29cf9e60feb1001a16bf06339a82976088e9cf
laura ursu <laura.ursu@madetech.com>
2025-11-24 13:25:12 +0000

    Refactoring to check if every Duty exist

--- test_themes.py
+++ test_themes.py
@@ -2,30 +2,15 @@ import unittest
 from themes import List_of_Duty
 
 class TestThemes(unittest.TestCase):
+
+    
     def test_duties_count(self):
         self.assertEqual(len(List_of_Duty), 13)
 
-    def test_duty_1(self):
-        Duty_1 = "Duty 1 Script and code in at least one general purpose language and at least one domain-specific language to orchestrate infrastructure, follow test driven development and ensure appropriate test coverage."
-        self.assertEqual(List_of_Duty[0], Duty_1)
+    def test_duty_contents(self):
+        for i, duty in enumerate(List_of_Duty, start=1):
+            self.assertTrue(duty.startswith(f"Duty {i}"))
     
-    def test_duty_2(self):
-        Duty_2 = "Duty 2 Initiate and facilitate knowledge sharing and technical collaboration with teams and individuals, with a focus on supporting development of team members."
-        self.assertEqual(List_of_Duty[1], Duty_2)
-    
-    def test_duty_12(self):
-        Duty_12 = "Duty 12 Look to automate any manual tasks that are repeated, often using APIs."
-        self.assertEqual(List_of_Duty[11], Duty_12)
-    
-    def test_duty_13(self):
-        Duty_13 = "Duty 13 Accept ownership of changes; embody the DevOps culture of 'you build it, you run it', with a relentless focus on the user experience."
-        self.assertEqual(List_of_Duty[12], Duty_13)
-        
-    
-    
-
-
-
 
 if __name__ == "__main__":
     unittest.main()
